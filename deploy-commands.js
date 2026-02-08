c9378482b23c759ea9e1168eb5cdffc06e81cf2b
karolbezak1-tech <karolbezak1@icloud.com>
2026-02-08 11:41:56 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,20 +4,27 @@ require('dotenv').config();
 const commands = [
     new SlashCommandBuilder()
         .setName('weryfikacja-panel')
-        .setDescription('Wysyła panel weryfikacyjny (tylko dla administracji) '),
+        .setDescription('Wysyła panel weryfikacyjny (tylko dla administracji)'),
+    
+    // NOWA KOMENDA REGULAMINU
+    new SlashCommandBuilder()
+        .setName('regulamin-panel')
+        .setDescription('Wysyła rozbudowany panel z regulaminem serwera'),
 ].map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
     try {
-        console.log('Rejestrowanie komend...');
+        console.log('Rozpoczynanie rejestracji komend slash...');
+        
         await rest.put(
             Routes.applicationCommands(process.env.CLIENT_ID),
             { body: commands },
         );
-        console.log('Komendy zarejestrowane pomyślnie!');
+        
+        console.log('Sukces! Wszystkie komendy (weryfikacja i regulamin) zostały zarejestrowane.');
     } catch (error) {
-        console.error(error);
+        console.error('Wystąpił błąd podczas rejestracji komend:', error);
     }
 })();
